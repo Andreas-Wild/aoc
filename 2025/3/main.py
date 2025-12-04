17c5e8c769d5aaf3e747b943787ffc202b0a0edb
andreas wild <andreaswild9@gmail.com>
2025-12-04 20:02:56 +0200

    Day 3 Part 1 complete

--- 2025/3/main.py
+++ 2025/3/main.py
@@ -2,13 +2,33 @@
 import time
 
 with open("test_input.txt", "r") as f:
-    test_input = f.read()
+    test_input = f.read().splitlines()
 with open("input.txt", "r") as f:
-    input = f.read()
+    input = f.read().splitlines()
 
 # Code Start
 answer = 0
-
+# So we need to check where the greatest two are. Easy step one solution is to sort and use [-2].
+for bank in input:
+    first, second = ("1", "1")
+    index = [-1, -1]
+    for i, digit in enumerate(bank):
+        # If digit is greater than current first place update and we still have digits left
+        if digit > first and i != len(bank) - 1:
+            index[0] = i
+            first = digit
+            # When we jump a ten, set second back to 1
+            second = "1"
+        # Else if digit is greater than the second place update
+        elif digit >= second:
+            index[1] = i
+            second = digit
+    if index[0] > index[1]:
+        best_batteries = second + first
+    else:
+        best_batteries = first + second
+    print(best_batteries)
+    answer += int(best_batteries)
 # Code End
 start_time = time.time()
 
